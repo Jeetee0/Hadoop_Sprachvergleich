8277d87fb9a4ec69730f0d053d55ca8b654f7df1
Youri Seichter <yseichter@gmail.com>
2019-06-17 20:00:05 +0200

    start to split the Mapper functions

---
+++ src/main/java/mappers/replaceMapper.java
@@ -0,0 +1,34 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.util.regex.*;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class ReplaceMapper extends Mapper<LongWritable, Text, LongWritable, Text> {
+
+    private Text word = new Text();
+    private long maxLength = 0;
+    private String longestWord = "";
+
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+        StringTokenizer itr = new StringTokenizer(value.toString());
+
+        while (itr.hasMoreTokens()) {
+            String currentToken = itr.nextToken().replaceAll("([\\p{Punct}])", "").trim().toLowerCase();
+            word.set(currentToken);
+
+            if (word.getLength() > maxLength) {
+                maxLength = word.getLength();
+                longestWord = word.toString();
+            }
+        }
+    }
+
+    protected void cleanup(Context context) throws IOException, InterruptedException {
+        context.write(new LongWritable(maxLength), new Text(longestWord));
+    }
+}
