b0f6ae9ea2f2e128000875a1bf7aa1de0ec449f1
Jeetee0 <oskar.sailer@sap.com>
2019-06-25 17:11:54 +0200

    update py script

--- src/main/python/python-sprachvergleich.py
+++ src/main/python/python-sprachvergleich.py
@@ -20,22 +20,26 @@ def find_longest_word_for_language(list):
     return longest_word
 
 
-# loop through folder
 if len(sys.argv) != 2:
-    print("Please give the root folder of your txt-files as an argument.")
+    print("INFO: Please give the root folder of your txt-files as an argument.")
     exit(1)
 
 rootdir = sys.argv[1]
-print("Counting words for all txt-files from root-folder: %s" % rootdir)
+print("INFO: Counting words for all txt-files from root-folder: '%s'" % rootdir)
+
 longest_words = []
 start = time.time()
+amount_of_files = 0
+
+# loop through folders
 for subdir, dirs, files in os.walk(rootdir):
+    print("INFO: Found %s amount of files in subfolder: '%s'" % (len(files), subdir))
+    amount_of_files += len(files)
     long_words = []
     for file in files:
         current_txt_file_path = os.path.join(subdir, file)
 
         # find longest word of file
-
         long_word = {}
         max_length = 0
         regex = r'\b\w+\b'
@@ -49,19 +53,16 @@ for subdir, dirs, files in os.walk(rootdir):
                         max_length = len(word)
 
         long_words.append(long_word)
+
+    # save longest word of language
     if len(long_words) > 0:
         longest_words.append(find_longest_word_for_language(long_words))
 
 
 end = time.time()
-print("Gesamte Laufzeit: %s secs" % str(end-start))
+print("FINISHED: Needed %s seconds to count words for %s files." % (str(end-start), amount_of_files))
 # print("Anzahl der Elemente: %s" % len(os.walk(rootdir)))
 
-print("Longest word for each language:")
+print("INFO: Longest word for each language:")
 for long_word in longest_words:
-    print("%s - %s - %s" % (long_word['language'], long_word['length'], long_word['word']))
-
-
-
-
-# aggregate into results file
+    print("\t%s - %s - %s" % (long_word['language'], long_word['length'], long_word['word']))
